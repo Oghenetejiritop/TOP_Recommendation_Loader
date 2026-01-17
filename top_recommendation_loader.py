0acec1adee028a49521da7911844149be65fa46a
Oghenetejiritop <tjsouthernking@gmail.com>
2026-01-16 21:50:33 -0500

    Initialising the NLP Pipeline

---
+++ top_recommendation_loader.py
@@ -0,0 +1,16 @@
+
+"""
+
+    TOP recommendation loader library implements a Spacy-based module to generate content recommendations using Natural Language Processing (NLP)  similarity techniques.
+
+    It reads content from file sources such as CSV, JSON, and other supported formats to analyse and recommend related content based on similarity levels.
+
+Author: Oghenetejiri Peace Onosajerhe.
+ """
+
+
+#imports#
+from spacy import load
+
+language_model = "en_core_web_lg"
+nlp = load(language_model)
